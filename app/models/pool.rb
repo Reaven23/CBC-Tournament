c53b2e01e295ae0b95dd0503a41443a5b58f78d1
Reaven23 <adrien.regis@gmail.com>
2025-09-09 00:42:29 +0200

    logic standings changed

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -34,28 +34,37 @@ class Pool < ApplicationRecord
   def standings
     # Règles de classement :
     # 1. Nombre de points (2 pour victoire, 1 pour défaite)
-    # 2. En cas d'égalité : confrontation directe
+    # 2. En cas d'égalité : confrontation directe (si applicable)
     # 3. Si 3+ équipes à égalité : goal average (différence de buts)
-    # 4. Si goal average identique : équipe qui a encaissé le moins de points
+    # 4. Si goal average identique : équipe qui a marqué le plus de points (meilleure attaque)
+    # Note: Si goal average ET attaque sont égaux, la défense sera forcément égale aussi
 
     teams.includes(:won_games).sort do |team_a, team_b|
       # 1. Comparaison par points
       points_diff = team_b.points - team_a.points
       next points_diff unless points_diff == 0
 
-      # 2. En cas d'égalité de points : confrontation directe
-      if team_a.head_to_head_victory?(team_b)
-        -1  # team_a gagne
-      elsif team_b.head_to_head_victory?(team_a)
-        1   # team_b gagne
-      else
-        # 3. Si pas de confrontation directe ou égalité : goal average
-        goal_diff = team_b.goal_difference - team_a.goal_difference
-        next goal_diff unless goal_diff == 0
-
-        # 4. Si goal average identique : équipe qui a encaissé le moins de points
-        team_a.goals_conceded - team_b.goals_conceded
+      # 2. En cas d'égalité de points : confrontation directe (seulement si 2 équipes)
+      # Pour 3+ équipes, on passe directement au goal average
+      if teams.count { |t| t.points == team_a.points } == 2
+        if team_a.head_to_head_victory?(team_b)
+          -1  # team_a gagne
+        elsif team_b.head_to_head_victory?(team_a)
+          1   # team_b gagne
+        end
       end
+
+      # 3. Si pas de confrontation directe applicable ou égalité : goal average
+      goal_diff = team_b.goal_difference - team_a.goal_difference
+      next goal_diff unless goal_diff == 0
+
+      # 4. Si goal average identique : équipe qui a marqué le plus de points (meilleure attaque)
+      goals_scored_diff = team_b.goals_scored - team_a.goals_scored
+      next goals_scored_diff unless goals_scored_diff == 0
+
+      # 5. Si tout est égal (points, goal average, attaque), alors défense est forcément égale
+      # Dans ce cas, on garde l'ordre alphabétique ou l'ordre d'insertion
+      0  # Égalité parfaite
     end
   end
 end
