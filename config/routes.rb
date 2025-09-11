01d9095706c1a503d0785746b5b29945bac17b28
Reaven23 <adrien.regis@gmail.com>
2025-09-11 12:34:06 +0200

    commited ERROR PAGES

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,11 @@ Rails.application.routes.draw do
   # Contact form
   post 'contact', to: 'notifications#create'
 
+  # Error pages
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unprocessable_entity'
+  get '/500', to: 'errors#internal_server_error'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
